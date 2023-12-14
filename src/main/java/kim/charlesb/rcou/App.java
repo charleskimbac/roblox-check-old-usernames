8a2ed4520e6c5c459de62c710a07197712eb1dad
charles k <52868068+charleskimbac@users.noreply.github.com>
2023-12-14 18:57:13 -0500

    remove help image, cleanup

--- src/main/java/kim/charlesb/rcou/App.java
+++ src/main/java/kim/charlesb/rcou/App.java
@@ -1,36 +1,41 @@
 package kim.charlesb.rcou;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.layout.HBox;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpClient.Version;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.net.http.HttpClient.Redirect;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.layout.Priority;
-import java.lang.Thread;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 
 /**
  * Application that takes in a Roblox username, converts the username
@@ -58,6 +63,8 @@ public class App extends Application {
     String username;
     long usernameID;
     String displayName;
+    CheckBox checkBox;
+    StackPane stackPane;
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
@@ -72,29 +79,35 @@ public class App extends Application {
         interactLayer.setAlignment(Pos.CENTER);
 
         usernameField = new TextField("username");
+        usernameField.setMinWidth(150);
 
         HBox.setHgrow(usernameField, Priority.ALWAYS);
 
         submitButton = new Button("Submit");
-        submitButton.setMaxHeight(12);
-        submitButton.setMinWidth(60);
+        submitButton.setMinHeight(12);
+        submitButton.setMinWidth(54);
+        submitButton.setMaxWidth(54);
         submitButton.setPadding(new Insets(4));
 
         imageView = new ImageView(new Image("file:resources/help.png"));
         imageView.setPreserveRatio(true);
         imageView.setFitHeight(14);
 
-        helpButton = new Button("", imageView);
-        helpButton.setPadding(new Insets(3));
-        helpButton.setMinWidth(24);
-        helpButton.setMinHeight(24);
+        helpButton = new Button("?"); // 
+        helpButton.setFont(Font.font(Font.getDefault().getName(), FontWeight.EXTRA_BOLD, 16));
+        helpButton.setPadding(new Insets(0));
+        helpButton.setMinWidth(25);
+        helpButton.setMaxWidth(25);
+        helpButton.setMinHeight(25);
+        helpButton.setMaxHeight(25);
 
         resultLayer = new VBox();
         resultLayer.setAlignment(Pos.CENTER);
 
         prevNameResults = new Label();
         prevNameResults.setStyle("-fx-text-alignment: center;");
-        prevNameResults.setManaged(false); // so initial app launch text is centered
+        // so initial app launch text is centered since this is empty
+        prevNameResults.setManaged(false);
 
         directionsTextFlow = new TextFlow();
         directionsTextFlow.setStyle("-fx-text-alignment: center;");
@@ -102,8 +115,7 @@ public class App extends Application {
         boldText = new Text("");
         boldText.setStyle("-fx-font-weight: bold;");
 
-        regularText = new Text("Submit a Roblox username to" +
-            "\n" + "see their previous usernames.");
+        regularText = new Text("Submit a Roblox username to" + "\n" + "see their previous usernames.");
 
         httpClient = HttpClient.newBuilder()
             .version(Version.HTTP_2)
@@ -116,31 +128,34 @@ public class App extends Application {
 
         pb = new ProgressBar();
         pb.setMaxHeight(10);
+        pb.setMinHeight(10);
         pb.setMaxWidth(Double.MAX_VALUE); // maxwidth is small by default
         HBox.setHgrow(pb, Priority.ALWAYS);
 
-        helpAlert = new Alert(AlertType.CONFIRMATION);
+        //ive tried to increase setWidth to make each tip be one line, but the width isnt working for the first show()
+        helpAlert = new Alert(AlertType.CONFIRMATION,
+            "- Submit a Roblox username (not their display name) to see their previous usernames." + "\n" +
+            "- Spaces entered into the search box will be converted into an underscore." + "\n" +
+            "- Press Enter while in the search box to instantly search.", 
+            ButtonType.OK
+        );
+        helpAlert.setTitle("Directions & Help");
+        helpAlert.setHeaderText("Directions & Help");
 
-        runnable = doAPIcalls();
+        runnable = doAPIcallsRunnable();
+
+        checkBox = new CheckBox("OCR");
+        checkBox.setPadding(new Insets(0, 7, 0 , 0));
+
+        stackPane = new StackPane();
+        StackPane.setAlignment(checkBox, Pos.CENTER_RIGHT);
     } // ApiApp
 
     /** {@inheritDoc} */
     @Override
     public void init() {
-        helpAlert.getButtonTypes().remove(ButtonType.CANCEL);
-        helpAlert.setWidth(600);
-        helpAlert.setHeight(240);
-        helpAlert.setTitle("Directions & Help");
-        helpAlert.setHeaderText("Directions & Help");
-        helpAlert.getDialogPane().setContentText("" +
-            "- Submit a Roblox username (not their display name) " +
-                "to see their previous usernames." + "\n" +
-            "- Spaces entered in the search box will be converted into an underscore." + "\n" +
-            "- Press Enter while in the search box " +
-                "to instantly search."
-        );
-
-        interactLayer.getChildren().addAll(usernameField, helpButton, submitButton);
+        stackPane.getChildren().addAll(usernameField, checkBox);
+        interactLayer.getChildren().addAll(stackPane, helpButton, submitButton);
         resultLayer.getChildren().addAll(directionsTextFlow, prevNameResults);
         directionsTextFlow.getChildren().addAll(boldText, regularText);
         root.getChildren().addAll(interactLayer, resultLayer, pb);
@@ -150,6 +165,12 @@ public class App extends Application {
                 Thread thread = new Thread(runnable);
                 thread.setDaemon(true);
                 thread.start();
+            } else if (event.getCode().getName().equals("ALT")) {
+                if (checkBox.isSelected() == true) {
+                    checkBox.setSelected(false);
+                } else {
+                    checkBox.setSelected(true);
+                }
             }
         });
 
@@ -160,8 +181,6 @@ public class App extends Application {
         }));
 
         helpButton.setOnAction((event) -> {
-            helpAlert.setWidth(600);
-            helpAlert.setHeight(240);
             helpAlert.show();
         });
     }
@@ -175,7 +194,6 @@ public class App extends Application {
         stage.setScene(scene);
         stage.sizeToScene();
         stage.show();
-        //System.out.println(submitButton.getHeight());
     } // start
 
     /**
@@ -184,11 +202,10 @@ public class App extends Application {
      * of the previous usernames for that userID. Presents the data accordingly.
      * @return Runnable for a thread to execute
      */
-    private Runnable doAPIcalls() {
+    private Runnable doAPIcallsRunnable() {
         return () -> {
             Platform.runLater(() -> { // default values
-                regularText.setStyle("");
-                boldText.setStyle("-fx-font-weight: bold;");
+                regularText.setStyle("");                
                 regularText.setManaged(true); // so everything is centered when regularText is empty
             });
             try {
@@ -202,8 +219,7 @@ public class App extends Application {
                         username += temp.charAt(i);
                     }
                 }
-                String requestBody = // required body per api doc
-                    "{\"usernames\": [\"" + username + "\"], \"excludeBannedUsers\": true}";
+                String requestBody = "{\"usernames\": [\"" + username + "\"], \"excludeBannedUsers\": true}";
                 HttpRequest requestUsernameToID = HttpRequest.newBuilder()
                     .uri(URI.create("https://users.roblox.com/v1/usernames/users"))
                     .header("Content-Type", "application/json")
@@ -231,8 +247,50 @@ public class App extends Application {
                 username = usernameResponse.data[0].name; // update to correct casing
                 usernameID = usernameResponse.data[0].id;
                 displayName = usernameResponse.data[0].displayName;
+
                 // second API call
-                getPreviousUsernames(); // method line limit exceeded
+                String usernameHistoryURI = "https://users.roblox.com/v1/users/" +
+                usernameID + "/username-history?limit=10&sortOrder=Asc";
+                
+                HttpRequest requestIDtoPrevNames = HttpRequest.newBuilder()
+                .uri(URI.create(usernameHistoryURI))
+                .build();
+                
+                HttpResponse<String> response2 =
+                httpClient.send(requestIDtoPrevNames, BodyHandlers.ofString());
+                
+                if (response2.statusCode() != 200) {
+                    Platform.runLater(() -> {
+                        boldText.setText("");
+                        regularText.setText("Error: status code " + response2.statusCode());
+                    });
+                    throw new Exception("error: status code " + response2.statusCode());
+                }
+                
+                PreviousNamesResponse previousNamesResponse =
+                gson.fromJson(response2.body(), PreviousNamesResponse.class);
+                
+                PreviousName[] pnrData = previousNamesResponse.data;
+                
+                // underline bold text also
+                Platform.runLater(setBoldTextRunnable(displayName, username));
+                
+                if (pnrData.length == 0) {
+                    Platform.runLater(() -> regularText.setText("\nNo previous usernames found."));
+                    throw new Exception("no previous usernames found");
+                }
+                
+                String prevNames = "";
+                for (int i = 0; i < pnrData.length; i++) {
+                    prevNames += pnrData[i].name + "\n";
+                }
+                
+                prevNameResults.setManaged(true);
+                Platform.runLater(prevNameResultsRunnable(prevNames));
+                //regularText.setStyle("-fx-underline: true;");
+                Platform.runLater(() -> regularText.setText(""));
+                regularText.setManaged(false);
+                //System.out.println(pnrData[0].name);
             } catch (Exception e) {
                 Platform.runLater(() -> {
                     prevNameResults.setText("");
@@ -243,6 +301,20 @@ public class App extends Application {
                 Platform.runLater(() -> stage.sizeToScene());
             }
         };
+    }
+
+    private String formatInput(String a) {
+        String fin = "";
+        if (checkBox.isSelected() == true) {
+            fin = separateNames(a);
+        }
+        return a;
+    }
+    
+    private String separateNames(String a) {
+        String fin = "";
+        
+        return a;
     }
 
     /**
@@ -265,54 +337,5 @@ public class App extends Application {
             boldText.setText(displayName + " (@" + username + ")");
             boldText.setStyle("-fx-underline: true; -fx-font-weight: bold;");
         };
-    }
-
-    /**
-     * Second API request that takes the user's userID from the first API request
-     * to then request for that userID's previous usernames. (+method exceeded line limit)
-     */
-    private void getPreviousUsernames() throws Exception {
-        String usernameHistoryURI = "https://users.roblox.com/v1/users/" +
-            usernameID + "/username-history?limit=10&sortOrder=Asc";
-
-        HttpRequest requestIDtoPrevNames = HttpRequest.newBuilder()
-            .uri(URI.create(usernameHistoryURI))
-            .build();
-
-        HttpResponse<String> response2 =
-            httpClient.send(requestIDtoPrevNames, BodyHandlers.ofString());
-
-        if (response2.statusCode() != 200) {
-            Platform.runLater(() -> {
-                boldText.setText("");
-                regularText.setText("Error: status code " + response2.statusCode());
-            });
-            throw new Exception("error: status code " + response2.statusCode());
-        }
-
-        PreviousNamesResponse previousNamesResponse =
-            gson.fromJson(response2.body(), PreviousNamesResponse.class);
-
-        PreviousName[] pnrData = previousNamesResponse.data;
-
-        // underline bold text also
-        Platform.runLater(setBoldTextRunnable(displayName, username));
-
-        if (pnrData.length == 0) {
-            Platform.runLater(() -> regularText.setText("\nNo previous usernames found."));
-            throw new Exception("no previous usernames found");
-        }
-
-        String prevNames = "";
-        for (int i = 0; i < pnrData.length; i++) {
-            prevNames += pnrData[i].name + "\n";
-        }
-
-        prevNameResults.setManaged(true);
-        Platform.runLater(prevNameResultsRunnable(prevNames));
-        //regularText.setStyle("-fx-underline: true;");
-        Platform.runLater(() -> regularText.setText(""));
-        regularText.setManaged(false);
-        //System.out.println(pnrData[0].name);
     }
 } // ApiApp
