c23c65f6f2e34e8a6cb9c5d6b1653b7afc25d542
charles k <52868068+charleskimbac@users.noreply.github.com>
2023-12-20 04:48:05 -0500

    save before tilepane impl. (multiple username search)

--- src/main/java/kim/charlesb/rcou/App.java
+++ src/main/java/kim/charlesb/rcou/App.java
@@ -23,7 +23,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
@@ -35,9 +37,9 @@ import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 /**
- * Application that takes in a Roblox username, converts the username
- * into their userID, then shows their previous usernames, if any exist.
- */
+* Application that takes in a Roblox username, converts the username
+* into their userID, then shows their previous usernames, if any exist.
+*/
 public class App extends Application {
     Stage stage;
     Scene scene;
@@ -46,47 +48,48 @@ public class App extends Application {
     TextField textField;
     Button submitButton;
     Button helpButton;
-    //Label prevNameResults;
     HttpClient httpClient;
     Gson gson;
     TextFlow directionsTextFlow;
     Text boldText;
     Text regularText;
     VBox resultLayer;
-    ProgressBar pb; //pb for aesthetics :)
+    ProgressBar progressBar; //progressBar for aesthetics :)
+    BorderPane progressBarLayer;
     Runnable runnable;
     Alert helpAlert;
     String username;
-    //long usernameID;
     String displayName;
     CheckBox checkBox;
     StackPane stackPane;
     String prevNames;
-
+    HBox checkBoxLayer;
+    Separator separator;
+    
     /**
-     * Constructs an {@code ApiApp} object. This default (i.e., no argument)
-     * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
-     */
+    * Constructs an {@code App} object. This default (i.e., no argument)
+    * constructor is executed in Step 2 of the JavaFX Application Life-Cycle.
+    */
     public App() {
         root = new VBox(6);
         root.setPadding(new Insets(4, 4, 3, 4));
         root.setAlignment(Pos.CENTER);
-
+        
         interactLayer = new HBox(4);
         interactLayer.setAlignment(Pos.CENTER);
-
+        
         textField = new TextField("username");
         textField.setMinWidth(150);
         textField.setMaxWidth(Double.MAX_VALUE); // maxwidth is small by default
-
+        
         HBox.setHgrow(textField, Priority.ALWAYS);
-
+        
         submitButton = new Button("Submit");
         submitButton.setMinHeight(12);
         submitButton.setMinWidth(54);
         submitButton.setMaxWidth(54);
         submitButton.setPadding(new Insets(4));
-
+        
         helpButton = new Button("?"); // 
         helpButton.setFont(Font.font(Font.getDefault().getName(), FontWeight.EXTRA_BOLD, 16));
         helpButton.setPadding(new Insets(0));
@@ -94,67 +97,80 @@ public class App extends Application {
         helpButton.setMaxWidth(25);
         helpButton.setMinHeight(25);
         helpButton.setMaxHeight(25);
-
+        
         resultLayer = new VBox();
         resultLayer.setAlignment(Pos.CENTER);
-/*
-        prevNameResults = new Label();
-        prevNameResults.setStyle("-fx-text-alignment: center;");
         
-        prevNameResults.setManaged(false); // so initial app launch text is centered since this is empty
-*/
         directionsTextFlow = new TextFlow();
         directionsTextFlow.setStyle("-fx-text-alignment: center;");
-
+        
         boldText = new Text("");
-        boldText.setStyle("-fx-font-weight: bold;");
-
+        //boldText.setStyle("-fx-font-weight: bold;");
+        boldText.setStyle("-fx-underline: true; -fx-font-weight: bold;");
+        
         regularText = new Text("Submit a Roblox username to" + "\n" + "see their previous usernames.");
-
+        
         httpClient = HttpClient.newBuilder()
-            .version(Version.HTTP_2)
-            .followRedirects(Redirect.NORMAL)
-            .build();
-
+        .version(Version.HTTP_2)
+        .followRedirects(Redirect.NORMAL)
+        .build();
+        
         gson = new GsonBuilder()
-            .setPrettyPrinting()
-            .create();
+        .setPrettyPrinting()
+        .create();
+        
+        progressBar = new ProgressBar();
+        progressBar.setMaxHeight(10);
+        progressBar.setMinHeight(10);
+        progressBar.setMaxWidth(Double.MAX_VALUE); // maxwidth is small by default
+        HBox.setHgrow(progressBar, Priority.ALWAYS);
 
-        pb = new ProgressBar();
-        pb.setMaxHeight(10);
-        pb.setMinHeight(10);
-        pb.setMaxWidth(Double.MAX_VALUE); // maxwidth is small by default
-        HBox.setHgrow(pb, Priority.ALWAYS);
-
-        //ive tried to increase setWidth to make each tip be one line, but the width isnt working for the first show()
+        progressBarLayer = new BorderPane();
+        progressBarLayer.setPadding(new Insets(5, 0, 0, 0));
+        VBox.setVgrow(progressBarLayer, Priority.ALWAYS);
+        
+        //tried to increase setWidth to make each tip be one line, but the width isnt working for the first show()
         helpAlert = new Alert(AlertType.CONFIRMATION,
-            "- Submit a Roblox username (not their display name) to see their previous usernames." + "\n" +
-            "- Spaces entered into the search box will be converted into an underscore." + "\n" +
-            "- Press Enter while in the search box to instantly search.", 
-            ButtonType.OK
+        "- Submit a Roblox username (not their display name) to see their previous usernames." + "\n" +
+        "- Press Enter while in the search box to instantly search." + "\n" +
+        "\n" +
+        "                     " + "-=[ SINGLE-SEARCH MODE (default) ]=-" + "\n" +
+        "- Spaces entered into the search box will be converted into an underscore." + "\n" +
+        "\n" +
+        "                                 " + "-=[ OCR MODE ]=-" + "\n",
+        ButtonType.OK
         );
         helpAlert.setTitle("Directions & Help");
         helpAlert.setHeaderText("Directions & Help");
-
+        
         runnable = doAPIcallsRunnable();
-
+        
         checkBox = new CheckBox("OCR");
-        checkBox.setPadding(new Insets(0, 7, 0, 0));
-
+        //checkBox.setPadding(new Insets(0, 7, 0, 0)); //7 away from left border of textField
+        
         stackPane = new StackPane();
         StackPane.setAlignment(checkBox, Pos.CENTER_RIGHT);
         HBox.setHgrow(stackPane, Priority.ALWAYS);
-    } // ApiApp
 
+        checkBoxLayer = new HBox(5);
+        checkBoxLayer.setPadding(new Insets(0, 8, 0, 8));
+
+        separator = new Separator();
+        separator.setPadding(new Insets(0, 4, 0, 4));
+        HBox.setHgrow(separator, Priority.ALWAYS);
+    } // ApiApp
+    
     /** {@inheritDoc} */
     @Override
     public void init() {
-        stackPane.getChildren().addAll(textField, checkBox);
-        interactLayer.getChildren().addAll(stackPane, helpButton, submitButton);
+        //stackPane.getChildren().addAll(textField, checkBox);
+        interactLayer.getChildren().addAll(textField, helpButton, submitButton);
         resultLayer.getChildren().addAll(directionsTextFlow); //prevNameResults);
         directionsTextFlow.getChildren().addAll(boldText, regularText);
-        root.getChildren().addAll(interactLayer, resultLayer, pb);
-
+        checkBoxLayer.getChildren().addAll(checkBox);
+        progressBarLayer.setBottom(progressBar);
+        root.getChildren().addAll(interactLayer, checkBoxLayer, separator, resultLayer, progressBarLayer);
+        
         textField.setOnKeyPressed(event -> {
             if (event.getCode().getName().equals("Enter")) {
                 Thread thread = new Thread(runnable);
@@ -168,18 +184,18 @@ public class App extends Application {
                 }
             }
         });
-
+        
         submitButton.setOnAction((event -> {
             Thread thread = new Thread(runnable);
             thread.setDaemon(true);
             thread.start();
         }));
-
+        
         helpButton.setOnAction((event) -> {
             helpAlert.show();
         });
     }
-
+    
     /** {@inheritDoc} */
     @Override
     public void start(Stage stage) {
@@ -190,76 +206,79 @@ public class App extends Application {
         stage.sizeToScene();
         stage.show();
     } // start
-
+    
     /**
-     * Returns a Runnable that takes in a user-specified username, then
-     * requests for the userID for that username. Then, requests for all
-     * of the previous usernames for that userID. Presents the data accordingly.
-     * @return Runnable for a thread to execute
-     */
+    * Returns a Runnable that takes in a user-specified username, then
+    * requests for the userID for that username. Then, requests for all
+    * of the previous usernames for that userID. Presents the data accordingly.
+    * @return Runnable for a thread to execute
+    */
     private Runnable doAPIcallsRunnable() {
         return () -> {
-            Platform.runLater(() -> { // default values
-                //regularText.setStyle("");                
-                //regularText.setManaged(true); // so everything is centered when regularText is empty
-                regularText.setText("Loading...");
-            });
+            submitButton.setDisable(true);
+            System.out.println(textField.getWidth());
+            //7chars !!!!
             try {
-                /*
-                //if user typed a space, change it to an underscore
-                String temp = textField.getText();
-                separateNames(textField.getText());
-
-                username = "";
-                for (int i = 0; i < temp.length(); i++) {
-                    if (temp.charAt(i) == ' ') {
-                        username += '_';
-                    } else {
-                        username += temp.charAt(i);
+                String inputFormatted = "";
+                if (checkBox.isSelected()) {
+                    System.out.println("OCR mode");
+                    inputFormatted = separateNames(textField.getText());
+                } else {
+                    System.out.println("single-search mode");
+                    //if user typed a space, change it to an underscore
+                    String temp = textField.getText();
+                    
+                    username = "";
+                    for (int i = 0; i < temp.length(); i++) {
+                        if (temp.charAt(i) == ' ') {
+                            username += '_';
+                        } else {
+                            username += temp.charAt(i);
+                        }
                     }
+                    inputFormatted = "\"" + username + "\"";
                 }
-                */
-                String input = separateNames(textField.getText());
-                String requestBody = "{\"usernames\": [" + input + "], \"excludeBannedUsers\": true}";
+                String requestBody = "{\"usernames\": [" + inputFormatted + "], \"excludeBannedUsers\": true}";
                 HttpRequest usernameToIDRequest = HttpRequest.newBuilder()
-                    .uri(URI.create("https://users.roblox.com/v1/usernames/users"))
-                    .header("Content-Type", "application/json")
-                    .header("accept", "application/json")
-                    .POST(BodyPublishers.ofString(requestBody))
-                    .build();
+                .uri(URI.create("https://users.roblox.com/v1/usernames/users"))
+                .header("Content-Type", "application/json")
+                .header("accept", "application/json")
+                .POST(BodyPublishers.ofString(requestBody))
+                .build();
+
                 HttpResponse<String> response1 = httpClient.send(usernameToIDRequest, BodyHandlers.ofString());
                 if (response1.statusCode() != 200) {
-                    Platform.runLater(() -> {
-                        boldText.setText("");
-                        regularText.setText("Error: status code " + response1.statusCode());
-                    });
+                    setBoldText("");
+                    setRegularText("Error: status code " + response1.statusCode());
                     throw new Exception("error: status code " + response1.statusCode());
                 }
+
                 //System.out.println(response1.body());
                 UsernameResponse usernameResponse = gson.fromJson(response1.body(), UsernameResponse.class);
                 if (usernameResponse.data.length == 0) {
-                    Platform.runLater(() -> {
-                        boldText.setText("Username not found.");
-                        regularText.setText("Please check and try again.");
-                    });
-                    throw new Exception("username not found");
+                    setBoldText("Username not found.");
+                    setRegularText("Please check and try again.");
+                    throw new Exception("username not found: " + inputFormatted);
                 }
                 username = usernameResponse.data[0].name; // update to correct casing                
                 displayName = usernameResponse.data[0].displayName;
                 prevNames = "";
-
-                // second API call
-                //long usernameID = 0;
-                for (int i = 0; i < usernameResponse.data.length; i++) {
-                    //System.out.println("hi");
-                    getPrevNamesFromID(usernameResponse.data[i].id);
-                    //System.out.println("hi");
-                }
                 
+                // second API call
+                // for every user responded with
+                for (int i = 0; i < usernameResponse.data.length; i++) {
+                    getPrevNamesFromID(usernameResponse.data[i].id);
+                }
             } catch (Exception e) {
+                Platform.runLater(() -> {
+                    submitButton.setDisable(false);
+                    System.out.println("regularText: " + regularText.getText());
+                    stage.sizeToScene();
+                });
                 System.out.println(e);
+                System.out.println();
             } finally {
-                Platform.runLater(() -> stage.sizeToScene());
+                System.out.println();
             }
         };
     }
@@ -268,25 +287,25 @@ public class App extends Application {
         try {
             String usernameHistoryURI = "https://users.roblox.com/v1/users/" + usernameID + "/username-history?limit=10&sortOrder=Asc";
             HttpRequest prevNamesFromIDRequest = HttpRequest.newBuilder()
-                .uri(URI.create(usernameHistoryURI))
-                .build();
+            .uri(URI.create(usernameHistoryURI))
+            .build();
             HttpResponse<String> response2 = httpClient.send(prevNamesFromIDRequest, BodyHandlers.ofString());
             if (response2.statusCode() != 200) {
-                Platform.runLater(() -> {
-                    setBoldText("");
-                    regularText.setText("Error: status code " + response2.statusCode());
-                });
+                
+                setBoldText("");
+                setRegularText("Error: status code " + response2.statusCode());
+                
                 throw new Exception("error: status code " + response2.statusCode());
             }
             
             PreviousNamesResponse previousNamesResponse = gson.fromJson(response2.body(), PreviousNamesResponse.class);
             PreviousName[] pnrData = previousNamesResponse.data;
             
-            // underline bold text also
-            Platform.runLater(setBoldTextRunnable(displayName, username));
+            
+            setBoldText(displayName + " (@" + username + ")");
             
             if (pnrData.length == 0) {
-                Platform.runLater(() -> regularText.setText("No previous usernames found."));
+                setRegularText("No previous usernames found.");
                 throw new Exception("no previous usernames found");
             }
             
@@ -295,19 +314,18 @@ public class App extends Application {
             }
             prevNames += pnrData[pnrData.length - 1].name; //last index no "\n"
             
-            //prevNameResults.setManaged(true);
-            Platform.runLater(prevNameResultsRunnable(prevNames));
-            //regularText.setStyle("-fx-underline: true;");
-            //Platform.runLater(() -> regularText.setText(""));
-            //regularText.setManaged(false);
-            //System.out.println(pnrData[0].name);
+            setRegularText(prevNames);
         } catch (Exception e) {
             System.out.println(e);
         } finally {
-            Platform.runLater(() -> stage.sizeToScene());
+            Platform.runLater(() -> {
+                submitButton.setDisable(false);
+                System.out.println("regularText: " + regularText.getText());
+                stage.sizeToScene();
+            });
         }
     }
-
+    
     private String formatInput(String a) {
         String fin = "";
         if (checkBox.isSelected() == true) {
@@ -315,7 +333,7 @@ public class App extends Application {
         }
         return a;
     }
-
+    
     private String separateNames(String a) {
         String fin = "";
         try {
@@ -332,7 +350,7 @@ public class App extends Application {
                     fin += "\"" + a.substring(start, end) + "\", "; // {"name", }
                     a = a.substring(end, a.length());
                     start = a.indexOf("@") + 1;
-                    System.out.println(fin);
+                    //System.out.println(fin);
                 }
                 fin = fin.substring(0, fin.length() - 2); //remove last ", "
                 System.out.println("done: " + fin);
@@ -343,41 +361,20 @@ public class App extends Application {
         }
         return fin;
     }
-
+    
     private static boolean isAtSign(char a) {
         if (a == '@') {
             return true;
         }
         return false;
     }
-    /**
-     * Returns a Runnable that sets prevNameResults to the previous usernames received.
-     * @param prevNames the String that contains the previous usernames
-     * @return a Runnable that sets prevNameResults to the previous usernames received
-     */
-    private Runnable prevNameResultsRunnable(String prevNames) {
-        return () -> regularText.setText(prevNames); //prevNameResults.setText(prevNames);
-    }
-
-    /**
-     * Returns a Runnable that sets boldText text and its style.
-     * @param displayName the given user's display name
-     * @param username the given user's display name
-     * @return Runnable that sets boldText text and its style
-     */
-    private Runnable setBoldTextRunnable(String displayName, String username) {
-        return () -> {
-            boldText.setText(displayName + " (@" + username + ")\n");
-            boldText.setStyle("-fx-underline: true; -fx-font-weight: bold;");
-        };
-    }
-
+    
     private void setBoldText(String a) {
         Platform.runLater(() -> {
             boldText.setText(a + "\n");
         });
     }
-
+    
     private void setRegularText(String a) {
         Platform.runLater(() -> {
             regularText.setText(a);
